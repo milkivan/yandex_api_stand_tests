a938a5e2413e04c18002685ad48e2d2302fd7e19
Ivan Obukhov <obukhovstud@gmail.com>
2023-03-15 21:26:10 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://776a0662-7b54-4065-a1a2-1f142b54a7da.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ main.py
@@ -0,0 +1,4 @@
+python
+ def test2_sum():
+     sum = 2 + 2
+     assert sum == 5
